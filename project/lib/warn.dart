ede29a446d05bb0c931b9ce1fcafa19937d0c5cc
Khaokla015 <92082259+Khaokla015@users.noreply.github.com>
2021-12-16 00:18:34 +0700

    หน้าแจ้งเตือน

---
+++ project/lib/warn.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class warn extends StatefulWidget {
+  @override
+  _warnState createState() => _warnState();
+}
+
+class _warnState extends State<warn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('การแจ้งเตือน'),
+        backgroundColor: Colors.teal[100],
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Row(children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start, 
+          children: <Widget>[
+            Text(
+              'พฤติกรรม',
+              style: TextStyle(fontSize: 32),
+            ),
+             SizedBox(
+                  height: 50,
+                ),
+            Text(
+              '       ปิดการใช้งานการสั่นเตือน',
+              style: TextStyle(fontSize: 32),
+            ),
+            SizedBox(
+                  height: 50,
+                ),
+            Text(
+              'เสียง',
+              style: TextStyle(fontSize: 32),
+            ),
+            SizedBox(
+                  height: 50,
+                ),
+            Text(
+              '       เปิดการใช้งานเสียง',
+              style: TextStyle(fontSize: 32),
+            ),
+          ])
+        ]),
+      ),
+    );
+  }
+}
