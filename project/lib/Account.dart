b301ff9d51d77f15c48e434e8c0d407884206c6e
Khaokla015 <92082259+Khaokla015@users.noreply.github.com>
2021-12-16 01:44:22 +0700

    เพิ่ม Account

---
+++ project/lib/Account.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'HomePage.dart';
+import 'important.dart';
+class account extends StatefulWidget {
+  @override
+  _accountState createState() => _accountState();
+}
+
+class _accountState extends State<account> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+        title: Text('บัญชีของฉัน'),
+        ),
+        body: Container(
+        padding: EdgeInsets.all(20),
+        child: Row(children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start, 
+          children: <Widget>[
+                    Text(
+                    'ข้อมูลบัญชี',
+                    style: TextStyle(color: Colors.black, fontSize: 40),
+                  ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => important()));
+                  },
+                  child: Text(
+                    '     ชื่อผู้ใช้',
+                    style: TextStyle(color: Colors.black, fontSize: 32),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => important()));
+                  },
+                  child: Text(
+                    '     เปลี่ยนอีเมล',
+                    style: TextStyle(color: Colors.black, fontSize: 32),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => important()));
+                  },
+                  child: Text(
+                    '     เปลี่ยนรหัสผ่าน',
+                    style: TextStyle(color: Colors.black, fontSize: 32),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => important()));
+                  },
+                  child: Text(
+                    '     ลบบัญชี',
+                    style: TextStyle(color: Colors.red, fontSize: 40),
+                  ),
+                ),
+          ]
+          )
+        ]
+        )
+        )
+        );
+  }
+}
